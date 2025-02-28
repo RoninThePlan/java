15db09e830435491018f0362ae4c73f1df2db4c6
admin <krzisio27@gmail.com>
2025-02-28 09:58:03 +0100

    haha Peg

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main
 {
     public static void main(String[] args) {
         Point p = new Point();
+        p.x = 1.8;
+        p.y = 3.6;
         System.out.println(p);
+        Point p1 = new Point();
+        Segment s = new Segment();
+        s.a=p;
+        s.b=p1;
+        System.out.println(s.length(p));
     }
    }
---
+++ src/Segment.java
@@ -0,0 +1,6 @@
+﻿public class Segment {
+    public Point a, b;
+    public double length() {
+        return Math.hypot(a.x - b.x, a.y - b.y);
+    }
+}
