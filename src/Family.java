5bd36e3913d65b5f0384d7d5eb35e42801be2f87
admin <krzisio27@gmail.com>
2025-03-28 13:22:05 +0100

    ima kms

--- src/Family.java
+++ src/Family.java
@@ -1,7 +1,7 @@
-﻿import java.util.Map;
-import java.util.HashMap;
+﻿import java.util.*;
+
 public class Family {
-    private Map<String,Map<String, List<Person>>> familyMembers;
+    private Map<String, List<Person>> familyMembers;
 
     public Family(){
         this.familyMembers=new HashMap<>();
@@ -13,12 +13,18 @@ public class Family {
  public void addMember(Person... p){
          for(Person p1:p){
              String key=p1.getName()+ " "+p1.getSurname();
-             this.familyMembers.put(key,p1);
+             this.familyMembers.computeIfAbsent(key,k->new ArrayList<Person>())
+             this.familyMembers.get(key).add(p1);
          }
 
 
  }
     public Person get(String key){
-        return this.familyMembers.get(key);
+     List<Person> people=this.familyMembers.get(key);
+        if(people==null||people.size()==0){
+        return null;}
+        Collections.sort(people);
+        people.sort(Comparator.reverseOrder());
+        return people.toArray(new Person[0])[0];
     }
 }
