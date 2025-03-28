6045df3c6214541c60a1908a6b4ca8be0996210d
admin <krzisio27@gmail.com>
2025-03-28 13:14:10 +0100

    ima kms

--- src/Family.java
+++ src/Family.java
@@ -6,10 +6,18 @@ public class Family {
     public Family(){
         this.familyMembers=new HashMap<>();
     }
-    public void addMember(Person p){
+ /*   public void addMember(Person p){
         String key=p.getName()+ " "+p.getSurname();
         this.familyMembers.put(key,p);
-    }
+    }*/
+ public void addMember(Person... p){
+         for(Person p1:p){
+             String key=p1.getName()+ " "+p1.getSurname();
+             this.familyMembers.put(key,p1);
+         }
+
+
+ }
     public Person get(String key){
         return this.familyMembers.get(key);
     }
